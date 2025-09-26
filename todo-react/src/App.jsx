465cc8e45101a66eec7871ded53830b9e46fd193
m-sheridan93 <m.sheridan93@gmail.com>
2025-09-26 17:38:30 +0200

    begin set up of input field in to do app

--- todo-react/src/App.jsx
+++ todo-react/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from "react";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [todoList, setTodoList] = useState([]);
+    const [newTask, setNewTask] = useState('');
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const addTask = () => {
+        const newTodoList = [...todoList, newTask]; // allows to expand an array or object into individual elements.
+        setTodoList(newTodoList);
+        setNewTask(''); // Clear the input field
+        console.log(JSON.stringify(todoList))
+    };
+
+    return (
+        <main>
+            <h1>To-Do</h1>
+            {
+                <label>
+                    <input name="myInput" placeholder="Add a To-Do Here"/>
+                    <button type="submit" onClick={addTask}>Add</button>
+                    <br/>
+                    <ul>
+                        <li>{todoList}</li>
+                    </ul>
+                </label>
+            }
+        </main>
+    );
 }
 
-export default App
+export default App;
