12b7c5dc85459fa268876919a45cac3d669e6b33
m-sheridan93 <m.sheridan93@gmail.com>
2025-10-07 17:53:25 +0200

    followed tutorial to help with react learning

--- todo-react/src/App.jsx
+++ todo-react/src/App.jsx
@@ -1,32 +1,8 @@
-import {useState} from "react";
+import ToDoList from "./ToDoList.jsx";
 import './App.css'
 
 function App() {
-    const [todoList, setTodoList] = useState([]);
-    const [newTask, setNewTask] = useState('');
-
-    const addTask = () => {
-        const newTodoList = [...todoList, newTask]; // allows to expand an array or object into individual elements.
-        setTodoList(newTodoList);
-        setNewTask(''); // Clear the input field
-        console.log(JSON.stringify(todoList))
-    };
-
-    return (
-        <main>
-            <h1>To-Do</h1>
-            {
-                <label>
-                    <input name="myInput" placeholder="Add a To-Do Here"/>
-                    <button type="submit" onClick={addTask}>Add</button>
-                    <br/>
-                    <ul>
-                        <li>{todoList}</li>
-                    </ul>
-                </label>
-            }
-        </main>
-    );
+return (<ToDoList />)
 }
 
 export default App;
